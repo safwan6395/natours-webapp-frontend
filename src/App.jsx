d99ef12cf284edeaa6c0e28a67b0386812147f5b
Safwan Hassan <safwan.hassan.d@gmail.com>
2023-08-06 16:14:46 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>a simple div</div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
